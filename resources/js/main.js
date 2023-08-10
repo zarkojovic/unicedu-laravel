74f3c355b3ad0667471ceadd89c508dc24cafb5f
Tigran <filipovtigran@gmail.com>
2023-08-10 13:34:26 +0200

    new js for profile

--- resources/js/main.js
+++ resources/js/main.js
@@ -5,28 +5,31 @@ document.addEventListener('DOMContentLoaded', function() {
     const btnSave = document.getElementById('btnSave');
     const btnCancel = document.getElementById('btnCancel');
 
-    const dobInput = document.getElementById('dobInput');
-    const citizenshipInput = document.getElementById('citizenshipInput');
-    const phoneInput = document.getElementById('phoneInput');
-    const passportInput = document.getElementById('passportInput'); 
-    const nameInput = document.getElementById('nameInput');
-    const surnameInput = document.getElementById('surnameInput');
-    
-    const displayDOB = document.getElementById('displayDOB');
-    const displayCitizenship = document.getElementById('displayCitizenship');
-    const displayPhone = document.getElementById('displayPhone');
-    const displayPassport = document.getElementById('displayPassport'); 
-    const displayName = document.getElementById('displayName');
-    const displaySurname = document.getElementById('displaySurame');
-    
-    
+    const inputs = {
+        dob: document.getElementById('dobInput'),
+        citizenship: document.getElementById('citizenshipInput'),
+        phone: document.getElementById('phoneInput'),
+        passport: document.getElementById('passportInput'),
+        name: document.getElementById('nameInput'),
+        surname: document.getElementById('surnameInput')
+    };
+
+    const display = {
+        dob: document.getElementById('displayDOB'),
+        citizenship: document.getElementById('displayCitizenship'),
+        phone: document.getElementById('displayPhone'),
+        passport: document.getElementById('displayPassport'),
+        name: document.getElementById('displayName'),
+        surname: document.getElementById('displaySurname')
+    };
+
     btnSave.addEventListener('click', function() {
         userForm.classList.add('d-none');
         displayForm.classList.remove('d-none');
         btnCancel.classList.add('d-none');
         btnSave.classList.add('d-none');
         btnEdit.classList.remove('d-none');
-        displayFormData();
+        updateDisplay();
     });
 
     btnEdit.addEventListener('click', function() {
@@ -35,15 +38,14 @@ document.addEventListener('DOMContentLoaded', function() {
         btnCancel.classList.remove('d-none');
         btnSave.classList.remove('d-none');
         btnEdit.classList.add('d-none');
-
     });
 
-    function displayFormData() {
-        displayName.textContent = nameInput.value;
-        displaySurname.textContent = surnameInput.value;
-        displayDOB.textContent = dobInput.value;
-        displayCitizenship.textContent = citizenshipInput.value;
-        displayPhone.textContent = phoneInput.value;
-        displayPassport.textContent = passportInput.value;
+    function updateDisplay() {
+        display.name.textContent = inputs.name.value;
+        display.surname.textContent = inputs.surname.value;
+        display.dob.textContent = inputs.dob.value;
+        display.citizenship.textContent = inputs.citizenship.value;
+        display.phone.textContent = inputs.phone.value;
+        display.passport.textContent = inputs.passport.value;
     }
-  });
+});
