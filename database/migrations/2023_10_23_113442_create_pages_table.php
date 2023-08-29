ccbd7292b53759c16028964de1c23313d4743376
zarkojovic <113613621+zarkojovic@users.noreply.github.com>
2023-08-29 14:59:02 +0200

    changed database and added logs

--- database/migrations/2023_10_23_113442_create_pages_table.php
+++ database/migrations/2023_10_23_113442_create_pages_table.php
@@ -15,6 +15,7 @@ return new class extends Migration {
             $table->string('route');
             $table->string('title');
             $table->string('icon');
+            $table->unsignedBigInteger('role_id');
             $table->timestamps();
         });
 
